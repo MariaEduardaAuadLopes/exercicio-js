4cae46cc4218e12e705429486f592f330990723d
MariaEduardaAuadLopes <maria.ealopes2@senacsp.edu.br>
2022-11-16 19:54:54 -0300

    atualizado ate o 6

---
+++ 005/app.js
@@ -0,0 +1,13 @@
+function Dividir(){
+
+    let valor1 = document.getElementById('n1').value
+
+
+
+let total= parseFloat(valor1) / parseFloat(100)
+
+document.getElementById('resultado').innerHTML = total
+
+}
+
+
